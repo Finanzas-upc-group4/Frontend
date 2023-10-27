f64e6bbd8f3b8978096b09b4411a0080e25d2773
Diego Santivañez <Diegojsf17@gmail.com>
2023-10-27 11:34:41 -0500

    feat: branch and registro-solicitud component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistroSolicitudComponent } from './Components/registro-solicitud/registro-solicitud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroSolicitudComponent
   ],
   imports: [
     BrowserModule
