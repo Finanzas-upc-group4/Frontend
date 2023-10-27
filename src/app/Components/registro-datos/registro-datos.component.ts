f024f46490089deddad11ae9ac3a7f7de045c392
ChrisitanZ <u202011688@upc.edu.pe>
2023-10-27 11:52:32 -0500

    Commint Registro-Datos

---
+++ src/app/Components/registro-datos/registro-datos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-datos',
+  templateUrl: './registro-datos.component.html',
+  styleUrls: ['./registro-datos.component.css']
+})
+export class RegistroDatosComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistroDatosComponent } from './Components/registro-datos/registro-datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroDatosComponent
   ],
   imports: [
     BrowserModule
