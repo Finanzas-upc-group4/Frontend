bb50d2fdad917e138eed8a6d0f3c0f06839758b2
YeniAlvarado <89022347+YeniAlvarado@users.noreply.github.com>
2023-10-27 11:49:42 -0500

    created input-data-evaluation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputDataEvaluationComponent } from './input-data-evaluation/input-data-evaluation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputDataEvaluationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/input-data-evaluation/input-data-evaluation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-data-evaluation',
+  templateUrl: './input-data-evaluation.component.html',
+  styleUrls: ['./input-data-evaluation.component.css']
+})
+export class InputDataEvaluationComponent {
+
+}
