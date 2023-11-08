6b4391e8f586dc36ec60134e9671fc1febea0192
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-11-08 15:23:27 -0500

    update: Cronograma view implemented

--- src/app/Components/cronograma/cronograma.component.ts
+++ src/app/Components/cronograma/cronograma.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
@@ -9,7 +8,7 @@ import { MatSort } from '@angular/material/sort';
   styleUrls: ['./cronograma.component.css']
 })
 export class CronogramaComponent {
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
 
   datos = [
     { numero_pago: 1, fecha: "0/0/0/", saldo: 0, interes: 0, seguro_desg: 0, seguro_bien: 0, amortizacion: 0, envio_fisico: "0/0/0/", cuota: 0 },
@@ -24,6 +23,9 @@ export class CronogramaComponent {
   dataSource = new MatTableDataSource(this.datos);
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
+  }
+  ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
 }
