e9f736e8d6562b8445950e7fc96e2af56897df0f
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-11-13 20:42:26 -0500

    update: Cronograma view implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule} from "@angular/material/table";
 import {MatSortModule} from "@angular/material/sort";
 import {CronogramaComponent} from "./Components/cronograma/cronograma.component";
+import {MatButtonModule} from "@angular/material/button";
+import { MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -14,14 +16,18 @@ import {CronogramaComponent} from "./Components/cronograma/cronograma.component"
     RegistroSolicitudComponent,
     CronogramaComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatTableModule,
-    MatSortModule
+    imports: [
+        BrowserModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatTableModule,
+        MatSortModule,
+        MatIconModule,
+        MatButtonModule
 
-  ],
+
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
