5998ea2721d45e425f6f5f6b458cf0f1f634f87b
Diego Santivañez <Diegojsf17@gmail.com>
2023-11-18 14:58:41 -0500

    feat: registro-solicitud implemented

--- src/app/Components/registro-solicitud/registro-solicitud.component.ts
+++ src/app/Components/registro-solicitud/registro-solicitud.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registro-solicitud',
@@ -6,11 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-solicitud.component.css']
 })
 export class RegistroSolicitudComponent {
+  constructor(private router: Router) {
+  }
   formData: any = {
-    creditCurrency: 'dolares'
+    creditCurrency: 'dolares',
+    disbursementDate: '',
+    payDay: '',
   };
 
   submitForm() {
-    console.log(this.formData);
+    // Generar un nombre único para el JSON
+    const uniqueFileName = 'solicitud_' + Date.now() + '.json';
+
+    // Crear un objeto JSON con los datos del formulario
+    const jsonData = {
+      creditCurrency: this.formData.creditCurrency,
+      disbursementDate: this.formData.disbursementDate,
+      payDay: this.formData.payDay,
+      UserId: 1,
+    };
+    localStorage.setItem('solicitud', JSON.stringify(jsonData));
+    localStorage.getItem('solicitud');
+    //console.log("esto es desde el local storage: "+localStorage.getItem('solicitud'));
+    const soli :any= (localStorage.getItem('solicitud'));
+    //const soli2 = JSON.parse(soli);
+    //localStorage.removeItem('solicitud')
+    //console.log("esto es desde el json despues del local" + soli);
+    //console.log(soli2.payDay)
+    // Imprimir el JSON en la consola para verificar
+    console.log(jsonData);
+    this.router.navigate(['/input-data-evaluation']);
   }
+
 }
