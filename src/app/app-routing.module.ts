be6daba0b7b00990247c4594d8ddd7b341cf38f6
Diego Santivañez <Diegojsf17@gmail.com>
2023-11-18 14:57:22 -0500

    feat: routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
 import { InputDataEvaluationComponent } from './input-data-evaluation/input-data-evaluation.component';
 import {RegistroDatosComponent} from "./Components/registro-datos/registro-datos.component";
+import {RegistroSolicitudComponent} from "./Components/registro-solicitud/registro-solicitud.component";
+import {
+    EvaluacionResultadosComponenteComponent
+} from "./Components/evaluacion-resultados-componente/evaluacion-resultados-componente.component";
+import {CronogramaComponent} from "./Components/cronograma/cronograma.component";
 
 const routes: Routes = [
     { path: 'input-data-evaluation', component: InputDataEvaluationComponent },
     {path: 'registrar-datos', component:RegistroDatosComponent},
     {path: 'login', component:RegistroDatosComponent},
     {path: 'register', component:RegistroDatosComponent},
-
+    {path: 'registro-solicitud', component: RegistroSolicitudComponent},
+    {path: 'resultado-evaluacion', component: EvaluacionResultadosComponenteComponent},
+    {path:'cronograma', component: CronogramaComponent}
 ];
 
 @NgModule({
